97f3824eb5fb9f0629e0726bf768678078ffd0fd
PiaLember <pia.lember@gmail.com>
2020-12-23 13:20:09 -0600

    Added API intergration

--- src/Summary.js
+++ src/Summary.js
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Summary() {
-  let weatherData = {
-    city: "Dallas",
-    description: "Cloudy",
-    temperature: "11",
-    realFeel: "8",
-    humidity: "0",
-    wind: "2",
-  };
-  return (
-    <div className="summary">
-      <div className="heading">
-        <h1 id="outcome">{weatherData.city}</h1>
-        <p id="description">{weatherData.description}</p>
-      </div>
-      <div className="description">
-        <h5 id="humidity">Humidity: {weatherData.humidity}%</h5>
-        <h5 id="wind">Wind speed: {weatherData.wind} m/s</h5>
-      </div>
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-      <img
-        src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-        alt="Clear"
-        className="float-left"
-        id="icon"
-      />
-      <h2 id="temp">{weatherData.temperature} F°</h2>
-      <p id="real-feel">Real Feel {weatherData.realFeel} F°</p>
-    </div>
-  );
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      feels: response.data.main.feels_like,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png#",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="summary">
+        <div className="heading">
+          <h1 id="outcome">{weatherData.city}</h1>
+          <p id="description">{weatherData.description}</p>
+        </div>
+        <div className="description">
+          <h5 id="humidity">Humidity: {Math.round(weatherData.humidity)}%</h5>
+          <h5 id="wind">Wind speed: {Math.round(weatherData.wind)} m/s</h5>
+        </div>
+
+        <img
+          src={weatherData.iconUrl}
+          alt="Clear"
+          className="float-left"
+          id="icon"
+        />
+        <div className="temperature">
+          <span className="temp">
+            {Math.round(weatherData.temperature)}
+            <span className="unit">°C</span>
+          </span>
+        </div>
+        <p id="real-feel">Real Feel {Math.round(weatherData.feels)}°</p>
+      </div>
+    );
+  } else {
+    const apiKey = "657e61445d4323be17aa0392f7a7870c";
+    let city = "Dallas";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
